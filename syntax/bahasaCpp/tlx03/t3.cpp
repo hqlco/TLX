7dec84459f1607e2227a883b57db8356d0c9b8d6
hq_lco <86828535+hqlco@users.noreply.github.com>
2021-09-10 11:34:45 +0700

    Create t3.cpp

---
+++ syntax/bahasaCpp/tlx03/t3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c,d,e;
+    cin>>a>>b;
+    c=a%b;
+    d=a-c;
+    e=d/b;
+    cout<<"masing-masing "<< e<< endl;
+    cout<<"bersisa " <<c<<endl;
+
+}
