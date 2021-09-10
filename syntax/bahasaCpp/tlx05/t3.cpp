08092f4308de567039b85aac3a560cd00158a06b
hq_lco <86828535+hqlco@users.noreply.github.com>
2021-09-10 11:38:08 +0700

    Create t3.cpp

---
+++ syntax/bahasaCpp/tlx05/t3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    cin>>a;
+    if (a>0)
+    {
+        cout<<"positif";
+    }
+    else if (a<0)
+    {
+        cout<<"negatif";
+    }
+    else
+    {
+        cout<<"nol";
+    }
+    
+    
+    return 0;
+}
