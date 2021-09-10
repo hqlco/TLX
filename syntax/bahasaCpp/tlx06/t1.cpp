41a5b4f75a8a57884384620b8ea7514101db8646
hq_lco <86828535+hqlco@users.noreply.github.com>
2021-09-10 11:40:18 +0700

    Create t1.cpp

---
+++ syntax/bahasaCpp/tlx06/t1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int N;
+    cin>>N;
+    int hasil;
+    
+    for (int i = 0; i <N ; i++)
+    {
+        int x;
+        cin>>x;
+        hasil +=x;
+        
+        
+        
+    }
+    cout<< hasil <<endl;
+    
+    
+    return 0;
+}
